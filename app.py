a1d33231c98c84d672f23eaf9642736bbc2569d1
bdrumheller <drumhb@rpi.edu>
2016-03-23 17:12:05 -0400

    json from mongo

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask.ext.pymongo import PyMongo
+from bson.json_util import dumps
 
 app = Flask(__name__)
 mongo = PyMongo(app)
 
 @app.route('/')
 def home_page():
-    return "test"
+    test = mongo.db.users.find({'online':True})
+    return dumps(test)
 
 if __name__ == '__main__':
     app.run(debug=True)
