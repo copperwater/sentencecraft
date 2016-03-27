dc0d6ffb7259eb3019035745c8cef730c6448fa5
bdrumheller <drumhb@rpi.edu>
2016-03-27 15:49:45 -0400

    view-sentences endpoint

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ from flask import Flask
 from flask.ext.pymongo import PyMongo
 from bson.json_util import dumps
 
-app = Flask(__name__)
+app = Flask("app")
 mongo = PyMongo(app)
 
-@app.route('/')
+
+# To manually insert sentences into the database
+# > mongo
+# > use app
+# > db.sentences.insert({"content": "Illuminati confirmed."})
+
+# To insert from a file
+# > "mongo < insert-sentences.txt"
+
+# View the API endpoint
+# > python app.py
+# > 127.0.0.1:5000/view-sentences
+
+@app.route('/view-sentences/')
 def home_page():
-    mongo.db.users.insert({"online": True})
-    test = mongo.db.users.find({'online':True})
+    test = mongo.db.sentences.find()
     return dumps(test)
 
 if __name__ == '__main__':
