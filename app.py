1180a8d136eb339b06d553904633726c38369a02
chetan7623k <chetan7623k@gmail.com>
2016-03-30 16:26:08 -0400

    Added sample code to display HTML file

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask import request # lets us access request parameters
 from flask.ext.pymongo import PyMongo
 from bson.json_util import dumps
-import flask
 
 app = Flask("app")
 mongo = PyMongo(app)
@@ -22,19 +21,6 @@ mongo = PyMongo(app)
 
 @app.route('/view-sentences/')
 def api_view_sentences():
-<<<<<<< HEAD
-    #count = request.args.get('count')
-    # TODO: use count to limit the number of sentences
-    sentences = mongo.db.sentences.find({'complete':True})
-    #return 'Your count was '+count+' '+dumps(sentences)
-    test = mongo.db.sentences.find()
-    return dumps(test)
-	
-@app.route('/view-HTML')
-def view_HTML_sample():
-	sentences = mongo.db.sentences.find()
-	return flask.render_template('index.html', sentences = sentences)
-=======
     count = request.args.get('count')
     if (count is None): count = '10'
 
@@ -43,7 +29,11 @@ def view_HTML_sample():
 
     sentences = mongo.db.sentences.find({'complete':True}).sort("_id", -1).limit(int(count))
     return 'Your count was '+count+' '+dumps(sentences)
->>>>>>> 4662d52022035e5bf60eb93d776de70da6eb9942
+
+@app.route('/view-HTML')
+def view_HTML_sample():
+	sentences = mongo.db.sentences.find()
+	return flask.render_template('index.html', sentences = sentences)
 
 if __name__ == '__main__':
     app.run(debug=True)
