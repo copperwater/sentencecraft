353ff88be4ec193298dbbfa9b9260d7a76aa526c
bdrumheller <drumhb@rpi.edu>
2016-03-31 16:10:26 -0400

    start incomplete sentences

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request # lets us access request parameters
 from flask.ext.pymongo import PyMongo
 from bson.json_util import dumps
+import uuid
 
 app = Flask("app")
 mongo = PyMongo(app)
@@ -32,10 +33,24 @@ def api_view_sentences():
 
 @app.route('/incomplete-sentence/')
 def api_get_incomplete_sentence():
-    sentence = mongo.db.sentences.find({'complete':False}).sort("_id", -1).limit(1)
-    return dumps(sentence)
+    sentence = mongo.db.sentences.find_one({'complete':False})    
+    try:
+        key = sentence['key']
+        return 'The key was: {0}'.format(key) + dumps(sentence)
+    except:
+        return "ERROR: no key found"
 
-@app.route('/view-HTML')
+@app.route('/start-sentence/', methods =['POST'])
+def api_start_incomplete_sentence():
+    sentence_start =request.form["sentence_start"]
+    words = sentence_start.split(' ')
+    key = uuid.uuid4()
+    mongo.db.sentences.insert({"words": words, "complete": False, "key": key})
+    return sentence_start
+    #TODO enforce the schema
+    #return errors if stuff goes wrong
+
+@app.route('/view-HTML/')
 def view_HTML_sample():
     sentences = mongo.db.sentences.find()
     return flask.render_template('index.html', sentences = sentences)
