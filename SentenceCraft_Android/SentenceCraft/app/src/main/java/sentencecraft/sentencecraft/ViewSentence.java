35a7f6c864c2b810b1396557324ea5cac830bd14
zqiu <zqiu1994@gmail.com>
2016-04-17 14:27:10 -0400

    added proof of concept button that can modify itself upon recieving an action

--- SentenceCraft_Android/SentenceCraft/app/src/main/java/sentencecraft/sentencecraft/ViewSentence.java
+++ SentenceCraft_Android/SentenceCraft/app/src/main/java/sentencecraft/sentencecraft/ViewSentence.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class ViewSentence extends AppCompatActivity {
 
@@ -24,6 +27,16 @@ public class ViewSentence extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void changetext(View view){
+        String me = "";
+        Random myrand = new Random();
+        for(int i = 0; i < 20; ++i){
+            me += myrand.nextInt(10);
+        }
+        Button but = (Button) findViewById(R.id.test);
+        but.setText(me);
     }
 
 }
