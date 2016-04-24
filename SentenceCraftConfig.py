b73dcece2d9662b7443510b5351a155dff786e08
aosdict <aosdict@gmail.com>
2016-04-24 12:40:11 -0400

    Added config file to hold parameters

---
+++ SentenceCraftConfig.py
@@ -0,0 +1,9 @@
+'''
+Web app configuration options.
+'''
+
+# Number of seconds a lexeme collection is active and can be changed.
+lexeme_collection_active_time = 300
+
+# Delay in seconds between checks for expired active lexeme collections.
+polling_delay = 2
