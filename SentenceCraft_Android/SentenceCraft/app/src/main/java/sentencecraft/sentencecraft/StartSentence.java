169a7cc56751df7ad1e429855aaf6fe5d0163d49
zqiu <zqiu1994@gmail.com>
2016-04-24 15:03:53 -0400

    implemented back button functionality

--- SentenceCraft_Android/SentenceCraft/app/src/main/java/sentencecraft/sentencecraft/StartSentence.java
+++ SentenceCraft_Android/SentenceCraft/app/src/main/java/sentencecraft/sentencecraft/StartSentence.java
@@ -3,6 +3,7 @@ package sentencecraft.sentencecraft;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -17,6 +18,12 @@ public class StartSentence extends AppCompatActivity {
         setContentView(R.layout.activity_start_sentence);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
