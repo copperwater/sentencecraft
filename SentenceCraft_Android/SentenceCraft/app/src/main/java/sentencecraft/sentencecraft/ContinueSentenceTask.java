d4b555426cfe3e340c8b1721cde4053113a99531
zqiu <zqiu1994@gmail.com>
2016-04-27 11:50:30 -0400

    extended continuesentencetask to eventually send complete sentence to the server

--- SentenceCraft_Android/SentenceCraft/app/src/main/java/sentencecraft/sentencecraft/ContinueSentenceTask.java
+++ SentenceCraft_Android/SentenceCraft/app/src/main/java/sentencecraft/sentencecraft/ContinueSentenceTask.java
@@ -9,6 +9,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 
 /**
@@ -19,6 +22,8 @@ public class ContinueSentenceTask extends DownloadInfoTask {
     private int tagsId;
     private String key = "";
     private String operationName = "DownloadTask";
+    private String lexemeToAdd = "";
+    private String isComplete = "";
 
     public ContinueSentenceTask(View rootView, Context context, int editId, int tagsId) {
         super(rootView, context, editId);
@@ -26,13 +31,13 @@ public class ContinueSentenceTask extends DownloadInfoTask {
     }
 
     protected void onPostExecute(String result) {
-        if(key.equals("")){
+        if (key.equals("")) {
             if (getResponseCode() == 200) {
                 ArrayList<String> data = interpretContinue(result);
-                TextView sentence = (TextView)rootView.findViewById(editId);
-                sentence.setText(context.getString(R.string.continue_lexeme_part,data.get(0)));
-                TextView tags = (TextView)rootView.findViewById(tagsId);
-                tags.setText(context.getString(R.string.continue_tags_part,data.get(1)));
+                TextView sentence = (TextView) rootView.findViewById(editId);
+                sentence.setText(context.getString(R.string.continue_lexeme_part, data.get(0)));
+                TextView tags = (TextView) rootView.findViewById(tagsId);
+                tags.setText(context.getString(R.string.continue_tags_part, data.get(1)));
             } else {
                 Snackbar mySnackBar;
                 mySnackBar = Snackbar.make(rootView, context.getString(R.string.error_operation_not_complete, operationName), Snackbar.LENGTH_LONG);
@@ -47,23 +52,23 @@ public class ContinueSentenceTask extends DownloadInfoTask {
         ArrayList<String> toReturn = new ArrayList<>();
         String userdata = "";
         String tagdata = "";
-        try{
-            JSONObject reader= new JSONObject(data);
+        try {
+            JSONObject reader = new JSONObject(data);
             key = reader.getString("key");
             JSONObject lexemeCollection = reader.getJSONObject("lexemecollection");
             JSONArray lexemes = lexemeCollection.getJSONArray("lexemes");
-            for(int i = 0; i < lexemes.length(); ++i){
+            for (int i = 0; i < lexemes.length(); ++i) {
                 userdata += lexemes.getString(i) + " ";
             }
-            try{
+            try {
                 JSONArray tags = lexemeCollection.getJSONArray("tags");
-                for(int i = 0; i < tags.length(); ++i){
-                    if(!tagdata.equals("")){
+                for (int i = 0; i < tags.length(); ++i) {
+                    if (!tagdata.equals("")) {
                         tagdata += ",";
                     }
                     tagdata += tags.getString(i);
                 }
-            }catch(JSONException e){
+            } catch (JSONException e) {
                 tagdata = "none";
             }
         } catch (JSONException e) {
@@ -74,4 +79,32 @@ public class ContinueSentenceTask extends DownloadInfoTask {
         return toReturn;
     }
 
+    @Override
+    protected String doInBackground(String... urls) {
+        if(urls.length == 2){
+            //assuming getting incomplete sentence
+            key = "";
+            return super.doInBackground(urls);
+        }else if(urls.length == 4){
+            lexemeToAdd = urls[2];
+            isComplete = urls[3];
+            return super.doInBackground(urls);
+        }else{
+            return "arguments not recognized";
+        }
+    }
+
+    @Override
+    protected void sendAdditionalData(HttpURLConnection conn) throws IOException {
+        if(key.equals("")){
+            super.sendAdditionalData(conn);
+        }else{
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            wr.writeBytes("sentence_addition=" + lexemeToAdd);
+            wr.writeBytes("&complete=" + isComplete);
+            wr.writeBytes("&key=" + key);
+            wr.flush();
+            wr.close();
+        }
+    }
 }
