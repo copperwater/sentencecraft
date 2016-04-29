53a1e0816b243f90a108d4b86e6905f6d216d5d3
Tausif Ahmed <atausif00@gmail.com>
2016-04-28 22:07:42 -0400

    Last of comments

--- SentenceCraft_IOS/SentenceCraft/ServerRequest.swift
+++ SentenceCraft_IOS/SentenceCraft/ServerRequest.swift
@@ -17,19 +17,23 @@ class ServerRequest {
 //		serverURL =  "http://128.113.151.26:5000/"
 	}
 	
+	// Function to send a Start Lexeme request to the server
 	func sendStartSentenceRequest(tags: String, sentence: String, type: String) {
+		// Create the request
 		let requestURL = NSURL(string: serverURL + "start/")
 		let request = NSMutableURLRequest(URL:requestURL!);
 
 		request.HTTPMethod = "POST"
 		
+		// Format the info so the server can handle it
 		let postString: String = "tags=\(tags)&start=\(sentence)&type=\(type)"
 		let postData: NSData = postString.dataUsingEncoding(NSASCIIStringEncoding, allowLossyConversion: true)!
 		let postLength: String = "\(postData.length)"
 		request.setValue(postLength, forHTTPHeaderField: "Content-Length")
 		request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
 		request.HTTPBody = postData
-			
+		
+		// Send the task and wait for a response
 		let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
 			data, response, error in
 				
@@ -42,13 +46,15 @@ class ServerRequest {
 		task.resume()
 	}
 	
-	
+	// Function to request an incomplete lexeme from the server for the user to complete or continue
 	func requestIncompleteLexeme(type: String) -> [String: AnyObject]? {
+		// Create the request
 		let requestURL = NSURL(string: serverURL + "incomplete/?type=\(type)")
 		let request = NSMutableURLRequest(URL: requestURL!)
 		request.HTTPMethod = "GET"
 		var dict: [String:AnyObject] = [:]
 		
+		// Send the request and wait for a response
 		let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
 			data, response, error in
 			
@@ -56,6 +62,7 @@ class ServerRequest {
 				print("error=\(error)")
 				return
 			}
+			// Convert the response from JSON to a dictionary
 			dict = self.convertDataToDictionary(data!)!
 		}
 		task.resume()
@@ -63,13 +70,15 @@ class ServerRequest {
 		return dict
 	}
 	
-	
+	// Function to send the user input to the server API to continue or complete a sentence
 	func sendAppendRequest(appendage: String, key: String, action: String, type: String) {
+		// Create the request
 		let requestURL = NSURL(string: serverURL + "append/")
 		let request = NSMutableURLRequest(URL:requestURL!);
 		
 		request.HTTPMethod = "POST"
 		
+		// Format the content of the message for the server
 		let postString: String = "key=\(key)&addition=\(appendage)&type=\(type)&complete=\(action)"
 		let postData: NSData = postString.dataUsingEncoding(NSASCIIStringEncoding, allowLossyConversion: true)!
 		let postLength: String = "\(postData.length)"
@@ -77,6 +86,7 @@ class ServerRequest {
 		request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
 		request.HTTPBody = postData
 		
+		// Send the request and wait for a response from the server
 		let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
 			data, response, error in
 			
@@ -90,14 +100,16 @@ class ServerRequest {
 
 	}
 	
-	
+	// Function to send a view request to the server
 	func sendViewRequest(type: String, tags: String) -> [[String: AnyObject]]? {
+		// Create the request
 		let requestURL = NSURL(string: serverURL + "view/?type=\(type)&tags=\(tags)")
 		let request = NSMutableURLRequest(URL:requestURL!)
 		var dict: [[String:AnyObject]] = []
 		var isDictionary: Bool = true
 		request.HTTPMethod = "GET"
 		
+		// Send the request and wait for a response
 		let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
 			data, response, error in
 			
@@ -107,19 +119,23 @@ class ServerRequest {
 				return
 			}
 			
+			// Check the response string to see if it is a list of dictionaries
 			let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding) as! String
 			if responseString[responseString.startIndex] != "[" {
 				isDictionary = false
 			} else {
+				// If the JSON object is in the right format, convert it into a list of dictionaries
 				dict = self.convertDataToDictionaryList(data!)!
 			}
 
 		}
 		task.resume()
+		// Wait for the dictionary to be populated by the request
 		while(dict.count < 1) { if isDictionary == false {break} }
 		return dict
 	}
 	
+	// Convert JSON to list of dictionaries
 	func convertDataToDictionaryList(data: NSData) -> [[String:AnyObject]]? {
 		do {
 			return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [[String:AnyObject]]
@@ -128,6 +144,7 @@ class ServerRequest {
 		return []
 	}
 	
+	// Convert JSON to dictionary
 	func convertDataToDictionary(data: NSData) -> [String:AnyObject]? {
 		do {
 			return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String:AnyObject]
