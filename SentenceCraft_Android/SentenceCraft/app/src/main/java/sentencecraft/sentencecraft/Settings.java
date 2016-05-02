feabfbda964c0d290d1d54d56e3c9229f854abcb
zqiu <zqiu1994@gmail.com>
2016-05-02 18:10:44 -0400

    settings not changed to clickable text instead of switches. Icon changed to white

--- SentenceCraft_Android/SentenceCraft/app/src/main/java/sentencecraft/sentencecraft/Settings.java
+++ SentenceCraft_Android/SentenceCraft/app/src/main/java/sentencecraft/sentencecraft/Settings.java
@@ -1,11 +1,14 @@
 package sentencecraft.sentencecraft;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.CompoundButton;
-import android.widget.Switch;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
 
@@ -24,28 +27,67 @@ public class Settings extends AppCompatActivity {
             ab.setDisplayHomeAsUpEnabled(true);
         }
 
-        //set switch and listener for network switch
-        Switch networkSwitch = (Switch) findViewById(R.id.networkSwitch);
-        if(networkSwitch != null){
-            networkSwitch.setChecked(GlobalValues.networkIsLocalhost);
-            networkSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    GlobalValues.networkIsLocalhost = isChecked;
-                }
-            });
-        }
+        //display current settings
+        setNetworkAsLocalHost(GlobalValues.networkIsLocalhost);
+        setWordAsLexeme(GlobalValues.lexemeIsWord);
+    }
 
-        //set switch and listener for lexeme switch
-        Switch lexemeSwitch = (Switch) findViewById(R.id.lexemeswitch);
-        if(lexemeSwitch != null){
-            lexemeSwitch.setChecked(GlobalValues.lexemeIsWord);
-            lexemeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    GlobalValues.lexemeIsWord = isChecked;
-                }
-            });
+    //respond to user clicks
+    public void settingButtonResponse(View view){
+        boolean response = true;
+        switch(view.getId()){
+            case R.id.setting_remote:
+                //use remote if that view was clicked
+                response = false;
+            case R.id.settings_localhost:
+                GlobalValues.networkIsLocalhost = response;
+                setNetworkAsLocalHost(response);
+                break;
+            case R.id.setting_sentence:
+                //use sentences as the lexeme if it was clicked
+                response = false;
+            case R.id.setting_word:
+                GlobalValues.lexemeIsWord = response;
+                setWordAsLexeme(response);
+                break;
+            default:
+                Log.d(getString(R.string.app_name),"Don't know how to respond to button click");
+        }
+    }
+
+    //deals with correctly coloring the right text view options for networking
+    private void setNetworkAsLocalHost(boolean response){
+        Context context = getApplicationContext();
+        TextView localhost = (TextView) findViewById(R.id.settings_localhost);
+        TextView remote = (TextView) findViewById(R.id.setting_remote);
+        if(localhost == null || remote == null){
+            Log.d(getString(R.string.app_name),"could not find view");
+            return;
+        }
+        if(response){
+            localhost.setTextColor(ContextCompat.getColor(context, R.color.colorPrimary));
+            remote.setTextColor(ContextCompat.getColor(context, R.color.colorBlack));
+        }else{
+            localhost.setTextColor(ContextCompat.getColor(context, R.color.colorBlack));
+            remote.setTextColor(ContextCompat.getColor(context, R.color.colorPrimary));
+        }
+    }
+
+    //deals with correctly coloring the right text view options for lexemes
+    private void setWordAsLexeme(boolean response){
+        Context context = getApplicationContext();
+        TextView word = (TextView) findViewById(R.id.setting_word);
+        TextView sentence = (TextView) findViewById(R.id.setting_sentence);
+        if(word == null || sentence == null){
+            Log.d(getString(R.string.app_name),"could not find view");
+            return;
+        }
+        if(response){
+            word.setTextColor(ContextCompat.getColor(context, R.color.colorPrimary));
+            sentence.setTextColor(ContextCompat.getColor(context, R.color.colorBlack));
+        }else{
+            word.setTextColor(ContextCompat.getColor(context, R.color.colorBlack));
+            sentence.setTextColor(ContextCompat.getColor(context, R.color.colorPrimary));
         }
     }
 }
